466d293d6b7c22a573f703a8dd59e0ea0e257b95
yr-wan <33694349@qq.com>
2018-08-18 03:06:42 +0800

    commit

--- src/com/yrwan19/java/TestURL.java
+++ src/com/yrwan19/java/TestURL.java
@@ -1,11 +1,15 @@
 package com.yrwan19.java;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.URL;
 
-//URL:一个URL对象对应互联网上的一个资源
+// URL:统一资源定位符，一个URL对象对应互联网上的一个资源
+// 可以通过URL的对象调用其相应的方法，将此资源读取（“下载”）
 public class TestURL {
 	public static void main(String[] args) throws Exception {
-		// 1.创建url对象
+		// 1.可以通过URL的对象调用其相应的方法，将此资源读取（“下载”）
 		URL url = new URL("https://www.baidu.com/img/superlogo_c4d7df0a003d3db9b65e9ef0fe6da1ec.png");
 		/*
 		 * public String getProtocol() 获取该URL的协议名
@@ -23,5 +27,16 @@ public class TestURL {
 		System.out.println(url.getFile());
 		System.out.println(url.getRef());
 		System.out.println(url.getQuery());
+
+		// 如何将服务端的资源读取进来:openStream()
+		InputStream is = url.openStream();
+		FileOutputStream fos = new FileOutputStream(new File("baidulogo.jpg"));
+		byte[] b = new byte[1024];
+		int len;
+		while ((len = is.read(b)) != -1) {
+			fos.write(b, 0, len);
+		}
+		fos.close();
+		is.close();
 	}
 }
