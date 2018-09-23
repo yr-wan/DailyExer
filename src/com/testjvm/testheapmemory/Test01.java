3d8654b7e8b85a37ce063bcb887c98ba4a1a0744
yr-wan <33694349@qq.com>
2018-09-24 00:30:15 +0800

    commit

--- src/com/testjvm/testheapmemory/Test01.java
+++ src/com/testjvm/testheapmemory/Test01.java
@@ -1,13 +1,13 @@
 package com.testjvm.testheapmemory;
 
 import java.util.ArrayList;
-import java.util.List;
 
+// 运行参数 -Xms20m -Xmx20m -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError 
 public class Test01 {
 	byte[] byteArray = new byte[1 * 1024 * 1024];// 1MB
 
 	public static void main(String[] args) {
-		List<Test01> list = new ArrayList<>();
+		ArrayList<Test01> list = new ArrayList<>();
 		try {
 			for (int i = 1; i <= 40; i++) {
 				list.add(new Test01());
